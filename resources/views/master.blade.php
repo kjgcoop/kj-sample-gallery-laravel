0a3efc4fb84cc75bd6550476d302c0e16d2580c0
KJ Coop <git@kjcoop.com>
2019-08-22 10:55:32 -0700

    When I moved off layout.app view, I forgot to bring some of the scripts along with me. Upon restoring them. the ability to click the logout link was restored.

--- resources/views/master.blade.php
+++ resources/views/master.blade.php
@@ -1,11 +1,29 @@
-<html>
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
 <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- CSRF Token -->
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+
     <title>KJ's Sample Laravel Gallery</title>
+
+    <!-- Scripts -->
+    <script src="{{ asset('js/app.js') }}" defer></script>
+
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
     <script src="https://kit.fontawesome.com/2c245143cb.js"></script>
     <script src="/js/kj-gallery.js"></script>
+
+    <!-- Fonts -->
+    <link rel="dns-prefetch" href="//fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
+
+    <!-- Styles -->
+    <link href="/css/app.css" rel="stylesheet">
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
     <link rel="stylesheet" type="text/css" href="/css/kj-gallery.css">
-    <link href="http://localhost:8000/css/app.css" rel="stylesheet">
 </head>
 
 <body>

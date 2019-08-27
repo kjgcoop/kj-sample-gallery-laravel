fbd62e09886bdc5aa1178089457bcd7f8bdd0a41
KJ Coop <git@kjcoop.com>
2019-08-27 07:33:58 -0700

    Toggle the border around a selected image

--- public/js/kj-gallery.js
+++ public/js/kj-gallery.js
@@ -1,5 +1,6 @@
 $(document).ready(function(){
 
+    // Show an iamge
     $('#imgs li.still').click(function () {
         var filename = $(this).attr('data-file');
         var new_tgt  = 'kj_images/' + filename;
@@ -8,6 +9,7 @@ $(document).ready(function(){
         $('#exif_block').html($(this).html());
     });
 
+    // Show a video
     $('#imgs li.video').click(function () {
         var filename = $(this).attr('data-file');
         var new_tgt  = '<video controls><source data-file="' + filename + '" src="kj_images/' + filename + '" type="video/mp4">Your browser doesn\'t support the video tag.</video>';
@@ -15,5 +17,17 @@ $(document).ready(function(){
         $('#full').html(new_tgt);
     });
 
+    // Toggle highlighting when an iamge is clicked
+    $('#imgs li').click(function () {
+        // Get rid of current highlighting
+        $('.selected').removeClass('selected');
+
+        // Add new highlighting
+        $(this).toggleClass('selected');
+        console.debug('Toggling');
+    });
+
+    // Display the first image
     $('#imgs li:first-child').click();
+
 });
